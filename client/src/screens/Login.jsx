ae2b48ebc6777fa54306e29e2d782e19d36b03e2
vaibhav-1098 <vaibhav.2003.rana@gmail.com>
2024-10-12 15:25:28 +0530

    _

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
+import { jwtDecode } from "jwt-decode";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import {jwtDecode} from "jwt-decode";
 import useDocumentTitle from "../hooks/useDocumentTitle";
 import { authActions } from "../redux/store";
 
